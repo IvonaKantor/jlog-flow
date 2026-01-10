edc13b08fe62da84a733a7f7868e23cab09d6e5d
ivona <ik87056@student.uwb.edu.pl>
2026-01-11 00:33:23 +0100

    update

--- jlog-server/src/main/java/com/logging/platform/DashboardResource.java
+++ jlog-server/src/main/java/com/logging/platform/DashboardResource.java
@@ -1,16 +1,11 @@
 package com.logging.platform;
 
-import jakarta.inject.Inject;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
-
-import com.logging.platform.entity.ExceptionEntity;
-import com.logging.platform.dto.ExceptionDto;
-import com.logging.platform.service.ExceptionService;
 
 
 @Path("/v1/log")
@@ -32,19 +27,6 @@ public class DashboardResource {
     @DELETE
     @Path("{id}")
     public void deleteLog(String id) {
-    }
-
-    @Inject
-    ExceptionService exceptionService;
-
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public ExceptionDto getSmt() {
-        ExceptionEntity entity = new ExceptionEntity();
-        entity.
-        entity.setMessage("Something went wrong");
-
-        return exceptionService.convertEntityToDto(entity);
     }
 }
 
