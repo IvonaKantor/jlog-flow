b867a7574aebc2d661ff4a20d95e7a3af8a7bec6
ivona <ik87056@student.uwb.edu.pl>
2026-01-15 16:11:22 +0100

    added entity mapper

---
+++ jlog-server/src/main/java/com/logging/platform/mapper/LogDataMapper.java
@@ -0,0 +1,11 @@
+package com.logging.platform.mapper;
+
+import com.logging.platform.entity.LogEntity;
+import com.logging.platform.models.LogData;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "cdi" )
+public interface LogDataMapper {
+    
+    LogEntity map(LogData source);
+}
