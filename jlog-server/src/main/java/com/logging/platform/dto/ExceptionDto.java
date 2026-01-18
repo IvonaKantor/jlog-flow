a6c89bef907bbbf0127c815fc5266a232a36de0d
ivona <ik87056@student.uwb.edu.pl>
2026-01-18 10:31:44 +0100

    added access func to dto classes

--- jlog-server/src/main/java/com/logging/platform/dto/ExceptionDto.java
+++ jlog-server/src/main/java/com/logging/platform/dto/ExceptionDto.java
@@ -11,4 +11,36 @@ public class ExceptionDto {
     private String exceptionType;
     private String message;
     private List<LogDataExceptionFrame> frames = new ArrayList<>();
+
+    public int getRefIid() {
+        return refIid;
+    }
+
+    public void setRefIid(int refIid) {
+        this.refIid = refIid;
+    }
+
+    public String getExceptionType() {
+        return exceptionType;
+    }
+
+    public void setExceptionType(String exceptionType) {
+        this.exceptionType = exceptionType;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public List<LogDataExceptionFrame> getFrames() {
+        return frames;
+    }
+
+    public void setFrames(List<LogDataExceptionFrame> frames) {
+        this.frames = frames;
+    }
 }
