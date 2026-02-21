a391c53ccabf1b638be67bf319465ce7237f6cb6
ivona <ik87056@student.uwb.edu.pl>
2026-02-21 23:04:10 +0100

    query param constraints

--- jlog-server/src/main/java/com/logging/platform/controller/LogController.java
+++ jlog-server/src/main/java/com/logging/platform/controller/LogController.java
@@ -2,6 +2,8 @@ package com.logging.platform.controller;
 
 import com.logging.platform.services.LogService;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.ws.rs.*;
 import org.openapi.quarkus.openapi_yaml.model.LogLevel;
 import org.openapi.quarkus.openapi_yaml.model.PaginationDataLog;
@@ -15,21 +17,18 @@ import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 public class LogController {
 
     @Inject
-    private LogService logService;
+    LogService logService;
 
     @GET
     @Produces(APPLICATION_JSON)
     @Consumes(APPLICATION_JSON)
     public PaginationDataLog getLogList(
-            @QueryParam("serviceId") Set<String> serviceIds,
+            @QueryParam("serviceId") @NotEmpty(message = "serviceId is required") Set<String> serviceIds,
             @QueryParam("serviceName") Set<String> serviceNames,
             @QueryParam("level") LogLevel level,
-            @QueryParam("pageIndex") @DefaultValue("0") int pageIndex,
+            @QueryParam("pageIndex") @DefaultValue("0") @Min(value = 0, message = "Page index must be >= 0") int pageIndex,
             @QueryParam("pageSize") @DefaultValue("20") int pageSize
     ) {
         return logService.getLogs(serviceIds, serviceNames, level, pageIndex, pageSize);
     }
-
 }
-
-
