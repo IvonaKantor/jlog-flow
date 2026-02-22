4eb5e69277835e9c2458a89be030ec2214948aac
ivona <ik87056@student.uwb.edu.pl>
2026-02-22 14:52:06 +0100

    query parameters constraints

--- jlog-server/src/main/java/com/logging/platform/controller/LogController.java
+++ jlog-server/src/main/java/com/logging/platform/controller/LogController.java
@@ -2,6 +2,7 @@ package com.logging.platform.controller;
 
 import com.logging.platform.services.LogService;
 import jakarta.inject.Inject;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.ws.rs.*;
@@ -23,11 +24,11 @@ public class LogController {
     @Produces(APPLICATION_JSON)
     @Consumes(APPLICATION_JSON)
     public PaginationDataLog getLogList(
-            @QueryParam("serviceId") @NotEmpty(message = "serviceId is required") Set<String> serviceIds,
+            @QueryParam("serviceId") Set<String> serviceIds,
             @QueryParam("serviceName") Set<String> serviceNames,
             @QueryParam("level") LogLevel level,
             @QueryParam("pageIndex") @DefaultValue("0") @Min(value = 0, message = "Page index must be >= 0") int pageIndex,
-            @QueryParam("pageSize") @DefaultValue("20") int pageSize
+            @QueryParam("pageSize") @DefaultValue("20") @Min(value = 0, message = "Page size must be >= 1") @Max(value = 500, message = "Page size must be less than 500") int pageSize
     ) {
         return logService.getLogs(serviceIds, serviceNames, level, pageIndex, pageSize);
     }
